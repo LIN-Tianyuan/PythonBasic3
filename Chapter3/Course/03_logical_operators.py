88f6144d21546b84c9b53e4f616c04216694a395
LIN-Tianyuan <47159399+LIN-Tianyuan@users.noreply.github.com>
2023-07-24 09:56:29 +0200

    Python Basic 3 week 5

---
+++ Chapter3/Course/03_logical_operators.py
@@ -0,0 +1,19 @@
+"""
+age = int(input("Veuillez entrer votre âge: "))
+if age > 0 and age < 120:
+    print("Âge correct.")
+else:
+    print("Âge incorrect.")
+
+
+python_score = int(input("Veuillez entrer votre score Python: "))
+c_score = int(input("Veuillez entrer votre score C: "))
+if python_score > 60 or c_score > 60:
+    print("Réussir l'examen.")
+else:
+    print("Rater l'examen.")
+"""
+
+is_employee = False
+if not is_employee:
+    print("Veuillez ne pas entrer!")
