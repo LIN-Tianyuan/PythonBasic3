3ad5f5b02683473d26896599c9cb95c01af51d17
LIN-Tianyuan <47159399+LIN-Tianyuan@users.noreply.github.com>
2023-07-31 10:40:18 +0200

    Python Basic 3 week 7

--- Chapter4/Course/01_while.py
+++ Chapter4/Course/01_while.py
@@ -3,10 +3,20 @@ i = 0
 while i < 10:
     print("Hello Python " + str(i))
     i = i + 1
-"""
+
 
 a = 0
 while a < 7:
     a = a + 1
     print(a)
+"""
+
+a = 0
+while a < 7:
+    print("La valeur de a est maintenant: " + str(a))
+    a = a + 1
+    print("La valeur de a après avoir ajouté 1 est: " + str(a))
+print(a)
+
+
 
---
+++ Chapter4/Course/exercise3.py
@@ -0,0 +1,7 @@
+sum = 0
+a = 0
+while a <= 100:
+    if a % 2 == 0:
+        sum = sum + a
+    a = a + 1
+print(sum)
